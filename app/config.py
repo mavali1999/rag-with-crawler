49442424855b53c780ba6cffa8cc4d8fff40e7ed
mavali <mohammadaminvali77@gmail.com>
2025-07-25 21:02:29 +0330

    tring first run

--- app/config.py
+++ app/config.py
@@ -2,6 +2,6 @@ import os
 
 DB_URL = os.environ["DB_URI"]
 
-HUGGING_FACE_API_KEY = os.environ["HUGGINGHUGGING_FACE_API_KEY"]
+HUGGING_FACE_API_KEY = os.environ["HUGGING_FACE_API_KEY"]
 PINECONE_API_KEY = os.environ["PINECONE_API_KEY"]
 
